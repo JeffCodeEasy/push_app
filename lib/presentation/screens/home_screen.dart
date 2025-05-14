f3b939e7bea2fda9f2f71515746639a6f16ce3be
Jeremy <jeffcode.easy@gmail.com>
2025-05-14 09:51:41 -0500

    first commit

---
+++ lib/presentation/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Permisos'),
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Placeholder(),
+    );
+  }
+}
